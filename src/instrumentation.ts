32cc29f6eb47944d8fbd54676b762d4b3695eaab
Di Yuan <diyuan@Di-Yuan.local>
2025-11-05 18:00:54 +0800

    Auto instrumentations

---
+++ src/instrumentation.ts
@@ -0,0 +1,25 @@
+import { NodeSDK } from "@opentelemetry/sdk-node";
+import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
+import { PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
+import { GraphQLInstrumentation } from "@opentelemetry/instrumentation-graphql";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
+import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-proto";
+
+const sdk = new NodeSDK({
+  traceExporter: new OTLPTraceExporter({
+    url: "http://otelcol:4318/v1/traces",
+  }),
+  metricReaders: [
+    new PeriodicExportingMetricReader({
+      exporter: new OTLPMetricExporter({
+        url: "http://otelcol:4318/v1/metrics",
+      }),
+    }),
+  ],
+  instrumentations: [
+    // getNodeAutoInstrumentations(),
+    new GraphQLInstrumentation(),
+  ],
+});
+
+sdk.start();
