41fe359cbf2e93b5ba0500a66a6dafdcffe3877d
Di Yuan <diyuan@Di-Yuan.local>
2025-11-05 18:20:01 +0800

    Manual metrics

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,8 +1,12 @@
-import { trace } from "@opentelemetry/api";
+import { metrics, trace } from "@opentelemetry/api";
 
 export const resolvers = {
   Query: {
     hello: (): string => {
+      const meter = metrics.getMeter("apollo-meter");
+      meter
+        .createCounter("request-count")
+        .add(1, { operation: "Query", field: "hello" });
       const tracer = trace.getTracer("apollo-server");
       const span = tracer.startSpan("hello-graphql-query");
       const result = "Hello from Apollo + OTel";
