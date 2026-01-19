55d31811a848972241fcdf5407e4a15d4da5bbb8
Di Yuan <diyuan@Di-Yuan.local>
2026-01-19 19:00:02 +0800

    GRPC otlp receiver

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -2,14 +2,14 @@ import { NodeSDK } from "@opentelemetry/sdk-node";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
 import { PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
 import { GraphQLInstrumentation } from "@opentelemetry/instrumentation-graphql";
-import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
-import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-proto";
 import { resourceFromAttributes } from "@opentelemetry/resources";
 import {
   ATTR_SERVICE_NAME,
   ATTR_SERVICE_VERSION,
 } from "@opentelemetry/semantic-conventions";
 import { metrics, trace } from "@opentelemetry/api";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-grpc";
+import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-grpc";
 
 const sdk = new NodeSDK({
   resource: resourceFromAttributes({
@@ -17,14 +17,14 @@ const sdk = new NodeSDK({
     [ATTR_SERVICE_VERSION]: "1.0",
   }),
   traceExporter: new OTLPTraceExporter({
-    url: "http://otelcol:4318/v1/traces",
+    url: "http://otelcol:4317/v1/traces",
   }),
   metricReaders: [
     new PeriodicExportingMetricReader({
       exporter: new OTLPMetricExporter({
-        url: "http://otelcol:4318/v1/metrics",
+        url: "http://otelcol:4317/v1/metrics",
       }),
-      exportIntervalMillis: 5000,
+      exportIntervalMillis: 10000,
     }),
   ],
   instrumentations: [
